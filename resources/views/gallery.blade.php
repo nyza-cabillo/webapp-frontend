0da0a9b6dba84b2517cd5436f1bebe12a32da9fe
nyza-cabillo <cabillonyza@gmail.com>
2022-11-13 14:04:55 +0800

    Added gallery page and setup blade directives

---
+++ resources/views/gallery.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+ 
+ 
+@section('content')
+
+     <!-- Masthead-->
+        <header class="masthead bg-primary text-white text-center">
+            <div class="container d-flex align-items-center flex-column">
+                <h1>Gallery</h1>
+            </div>
+        </header>
+
+@endsection
