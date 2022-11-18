e32e9a2602c67034d02d02155314ab547f711769
nyza-cabillo <cabillonyza@gmail.com>
2022-11-18 14:57:23 +0800

    Adding Login Page and addition og Dashboard Assets

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('homepage');
-});
+Route::get('/',  [PagesController::class, 'index'] );
 
-Route::get('/gallery', function () {
-    return view('gallery');
-});
+Route::get('/login', [PagesController::class, 'login']);
